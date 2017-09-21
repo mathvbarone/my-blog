abeacf4a8c849737f15a121cb75fab60d81cd322
Matheus Barone <matheusvbarone@gmail.com>
2017-09-21 12:25:14 -0300

    Ajustando caminho dos favicons

---
+++ _site/gulpfile.js
@@ -0,0 +1,136 @@
+var gulp           = require('gulp'),
+	plumber       = require('gulp-plumber'),
+	sass          = require('gulp-sass'),
+	autoprefixer  = require('gulp-autoprefixer'),
+	browserSync   = require('browser-sync'),
+	uglify        = require('gulp-uglify'),
+	concat        = require('gulp-concat'),
+	imagemin      = require('gulp-imagemin'),
+	rename        = require('gulp-rename'),
+	beautifycss   = require('gulp-cssbeautify'),
+	cssmin        = require('gulp-cssmin'),
+	cp            = require('child_process');
+
+
+/**
+ * Files Path
+ */
+
+var paths = {
+	src: {
+		sass: ['assets/src/sass/**/*.sass'],
+		js: 'assets/src/js/**/*.js',
+		img: 'assets/src/img/**/*'
+	},
+	dest: {
+		sass: 'assets/dist/css',
+		js: 'assets/dist/js',
+		img: 'assets/dist/images',
+		beauty: {
+			sass: 'assets/dist/css/beauty'
+		}
+	}
+	}
+
+
+
+var messages = {
+	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+	browserSync.notify(messages.jekyllBuild);
+	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
+		.on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+	browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['jekyll-build'], function() {
+	browserSync({
+		server: {
+			baseDir: '_site'
+		}
+	});
+});
+
+/**
+ * Stylus task
+ */
+
+gulp.task('css', function() {
+	gulp.src(paths.src.sass)
+	    .pipe(plumber())
+	    .pipe(sass())
+	    .pipe(autoprefixer({
+		   browsers: ['last 30 versions'],
+		   cascade: false
+	    }))
+	    .pipe(cssmin())
+	    .pipe(plumber.stop())
+	    .pipe(rename('style.min.css'))
+	    .pipe(gulp.dest(paths.dest.sass))
+	    .pipe(browserSync.reload({stream:true}))
+	    .pipe(beautifycss())
+	    .pipe(gulp.dest(paths.dest.beauty.sass))
+ });
+
+
+/**
+ * Javascript Task
+ */
+gulp.task('js', function() {
+	gulp.src(['src/js/jquery-3.2.1.js',
+			'src/js/wow.js',
+			'src/js/scrollify.js',
+			'src/js/typed.js',
+			'src/js/main.js'])
+	    .pipe(plumber())
+	    .pipe(concat('concat.js'))
+	    .pipe(gulp.dest(paths.dest.js))
+	    .pipe(rename('main.min.js'))
+	    .pipe(uglify())
+	    .pipe(plumber.stop())
+	    .pipe(gulp.dest(paths.dest.js));
+ });
+
+/**
+ * Imagemin Task
+ */
+gulp.task('imagemin', function() {
+	return gulp.src(paths.src.img)
+		.pipe(plumber())
+		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+		.pipe(gulp.dest(paths.dest.img));
+});
+
+
+/**
+ * Watch stylus files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+ gulp.task('watch', function () {
+	gulp.watch(paths.src.sass, ['css']);
+	gulp.watch(paths.src.js, ['js']);
+	gulp.watch('paths.src.{jpg,png,gif}', ['imagemin']);
+	gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['js', 'css', 'browser-sync', 'watch']);
