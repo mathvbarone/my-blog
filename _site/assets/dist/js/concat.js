d6fb27507b3c34d55822d60e35d51ce7528dfa4d
Matheus Barone <matheusvbarone@gmail.com>
2017-10-04 15:54:36 -0300

    Estilizando btn de retorno

--- _site/assets/dist/js/concat.js
+++ _site/assets/dist/js/concat.js
@@ -10542,11 +10542,11 @@ var postFilter = function(){
           }
           
           if(message == "success"){
-            var messageText = '<div class="success-box"><svg class="checkmark checkmark-success" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><strong>Sua mensagem foi enviada!</strong><button  type="button" title="retornar" class="form-return button">Retornar</a></div>';
+            var messageText = '<div class="success-box"><svg class="checkmark checkmark-success" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><strong>Sua mensagem foi enviada!</strong><button  type="button" title="retornar" class="form-return button is-primary">Retornar</a></div>';
             var ajaxStatus = "success";
           }
           if(message == "error"){
-            var messageText = '<div class="error-box"><svg class="checkmark  checkmark-error" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark-check" fill="none" d="M16 16 36 36 M36 16 16 36"/></svg><strong>Ocorreu um erro :/</strong><button type="button" title="retornar" class="form-return button">Retornar</a></div>';
+            var messageText = '<div class="error-box"><svg class="checkmark  checkmark-error" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark-check" fill="none" d="M16 16 36 36 M36 16 16 36"/></svg><strong>Ocorreu um erro :/</strong><button type="button" title="retornar" class="form-return button  is-primary">Retornar</a></div>';
             varajaxStatus = "error";
           }
     
