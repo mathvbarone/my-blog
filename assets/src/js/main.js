e79b2286a40a63c420ea08f86392c0fd200de7fa
Matheus Barone <matheusvbarone@gmail.com>
2017-10-09 13:46:03 -0300

    Ajustando links do menu

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -238,8 +238,7 @@ var postFilter = function(){
 (function() {
   var links = document.links;
   for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-    console.log(links[i].hostname, ("www." + window.location.hostname));
-    if (links[i].hostname != ("www." + window.location.hostname)) {
+    if (links[i].hostname != (window.location.hostname)) {
       links[i].target = '_blank';
       links[i].className += ' externalLink';
     }
